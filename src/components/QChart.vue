628e19394ee65ec109f8a8f1134bc41448265438
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-03-09 16:29:17 -0300

    Create componente QChart

---
+++ src/components/QChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas :id="identifier" :style="stilo"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+export default {
+  props: ["identifier", "stilo", "type", "labels", "datasets", "options"],
+  data() {
+    return {};
+  },
+  mounted() {
+    let ctx = document.getElementById(this.identifier);
+    let myChart = new Chart(ctx, {
+      type: this.type,
+      data: {
+        labels: this.labels,
+        datasets: this.datasets
+      },
+      options: this.options
+    });
+  }
+};
+</script>
