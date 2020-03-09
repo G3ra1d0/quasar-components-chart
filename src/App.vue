8b6afcf5e4c87ec99f96cfb33c89f0ceb4d35e7d
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-03-09 16:29:29 -0300

    Testando Componente QChart

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <q-chart
+      identifier="teste"
+      :stilo="style"
+      type="line"
+      :labels="labels"
+      :datasets="datasets"
+      :options="options"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QChart from "./components/QChart.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    QChart
+  },
+  data() {
+    return {
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)"
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)"
+          ],
+          borderWidth: 1
+        }
+      ],
+      style: {},
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    };
   }
-}
+};
 </script>
 
 <style>
